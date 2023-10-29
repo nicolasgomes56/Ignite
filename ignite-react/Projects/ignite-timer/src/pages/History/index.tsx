59b7bf4013ff0e65a35d9d225624f5bfa6eb9bc2
Nicolas Gomes <nicolasgomes56@gmail.com>
2023-10-29 10:26:29 -0300

    first commit

---
+++ ignite-react/Projects/ignite-timer/src/pages/History/index.tsx
@@ -0,0 +1,48 @@
+import { HistoryContainer, HistoryList } from './style'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Estudar React</td>
+              <td>1:30</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>1:30</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>1:30</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>1:30</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
