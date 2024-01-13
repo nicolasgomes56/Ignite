d2727ed0173b8ca43ef0e73397881cc9875a4587
Nicolas Gomes <nicolasgomes56@gmail.com>
2024-01-12 21:30:34 -0300

    Add title and description to Home page

--- ignite-react/Challegens/coffee-delivery/src/pages/Home/index.tsx
+++ ignite-react/Challegens/coffee-delivery/src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
-import { HomeContainer } from './style'
+import { HomeContainer, TitleContainer } from './style'
 
 import coffeeImg from '@/assets/coffee.png'
 
 export function Home() {
   return (
     <HomeContainer>
-      <p>Encontre o café perfeito para qualquer hora do dia</p>
-      <div></div>
+      <TitleContainer>
+        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+        <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+      </TitleContainer>
       <div>
         <img src={coffeeImg} alt="Café" />
       </div>
--- ignite-react/Challegens/coffee-delivery/src/pages/Home/style.ts
+++ ignite-react/Challegens/coffee-delivery/src/pages/Home/style.ts
@@ -2,14 +2,21 @@ import styled from 'styled-components'
 
 export const HomeContainer = styled.main`
   display: flex;
-
   padding: 2rem 10rem;
-
-  gap: 2rem;
+  gap: 16rem;
 
   background-color: ${({ theme }) => theme.colors.background};
+`
 
-  p {
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+
+  width: 588px;
+
+  h1 {
     font-family: 'Baloo 2', monospace;
     font-size: 3rem;
     font-weight: 800;
@@ -18,5 +25,14 @@ export const HomeContainer = styled.main`
     align-self: stretch;
 
     color: ${({ theme }) => theme.colors['base-title']};
+  }
+
+  p {
+    font-family: 'Roboto', monospace;
+    font-size: 1.25rem;
+    font-weight: 400;
+    line-height: 1.625rem;
+
+    color: ${({ theme }) => theme.colors['base-text']};
   }
 `
