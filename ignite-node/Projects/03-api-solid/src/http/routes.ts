e898b405eed2f0adc9f905f0790de134b2eb5596
Nicolas Gomes <nicolasgomes56@gmail.com>
2024-01-31 11:13:18 -0300

    fix

--- ignite-node/Projects/03-api-solid/src/http/routes.ts
+++ ignite-node/Projects/03-api-solid/src/http/routes.ts
@@ -2,5 +2,5 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/users', registaer)
+  app.post('/users', register)
 }
