189bcfc1197ee5b1bcd060b4a81b1146387f3562
Nicolas Gomes <nicolasgomes56@outlook.com>
2024-03-27 20:15:15 -0300

    Adiciona suporte ao pacote @fastify/jwt e implementa autenticação e perfil de usuário.

--- ignite-node/Projects/03-api-solid/src/env/index.ts
+++ ignite-node/Projects/03-api-solid/src/env/index.ts
@@ -4,6 +4,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'production', 'test']).default('production'),
   PORT: z.coerce.number().default(3333), // coerce converte o valor para o tipo especificado (number)
+  JWT_SECRET: z.string()
 })
 
 const _env = envSchema.safeParse(process.env)
--- ignite-node/Projects/03-api-solid/src/http/routes.ts
+++ ignite-node/Projects/03-api-solid/src/http/routes.ts
@@ -1,9 +1,15 @@
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 import { register } from './controllers/register'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   // Traduzir as rotas para formato de entidades (users -> users, sessions -> authenticate)
   app.post('/users', register)
+
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
