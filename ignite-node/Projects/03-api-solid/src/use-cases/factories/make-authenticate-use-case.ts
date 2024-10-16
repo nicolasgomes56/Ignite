ad249199b7a192049cf69633a7d7ebc9eb2f53f2
Nicolas Gomes <nicolasgomes56@gmail.com>
2024-10-16 18:41:05 -0300

    clean project & refactory all

--- ignite-node/Projects/03-api-solid/src/use-cases/factories/make-authenticate-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '../authenticate'
-
-export function makeAuthenticateUseCase() {
-  const userRepository = new PrismaUsersRepository()
-  const authenticateUseCase = new AuthenticateUseCase(userRepository)
-
-  return authenticateUseCase
-}
--- ignite-node/Projects/03-api-solid/src/use-cases/factories/make-search-gyms-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
-import { SearchGymUseCase } from '../search-gyms'
-
-export function makeSearchGymsUseCase() {
-  const gymsRepository = new PrismaGymsRepository()
-  const useCase = new SearchGymUseCase(gymsRepository)
-
-  return useCase
-}
