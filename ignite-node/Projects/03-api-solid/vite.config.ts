d0fd21d8e110c99a629f0a3bdff4f875faff4339
Nicolas Gomes <nicolasgomes56@gmail.com>
2024-11-08 18:26:37 -0300

    feat: adiciona suporte a testes E2E e middleware de verificação JWT, atualiza rotas e controllers para gerenciamento de perfil de usuário

--- ignite-node/Projects/03-api-solid/vite.config.ts
+++ ignite-node/Projects/03-api-solid/vite.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [tsconfigPaths()],
+	test: {
+		environmentMatchGlobs: [
+			['src/http/controllers/**', 'src/vitest-environments/prisma.ts'],
+		],
+		dir: 'src',
+	},
 });
