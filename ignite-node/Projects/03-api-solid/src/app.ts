74cb01cb2f79d95618c9ba6368a9b86b4cf668bd
Nicolas Gomes <nicolasgomes56@gmail.com>
2024-11-14 17:48:15 -0300

    feat: Adicionado a parte de Roles (RBAC), configurado o middleware, routes e testes com as roles, criado também os ultimos testes E2E e configuração do CI

--- ignite-node/Projects/03-api-solid/src/app.ts
+++ ignite-node/Projects/03-api-solid/src/app.ts
@@ -1,3 +1,4 @@
+import fastifyCookie from '@fastify/cookie';
 import fastifyJwt from '@fastify/jwt';
 import fastity from 'fastify';
 import { ZodError } from 'zod';
@@ -10,7 +11,16 @@ export const app = fastity();
 
 app.register(fastifyJwt, {
 	secret: env.JWT_SECRET,
+	cookie: {
+		cookieName: 'refreshToken',
+		signed: false,
+	},
+	sign: {
+		expiresIn: '10m',
+	},
 });
+
+app.register(fastifyCookie);
 
 app.register(usersRoutes);
 app.register(gymsRoutes);
